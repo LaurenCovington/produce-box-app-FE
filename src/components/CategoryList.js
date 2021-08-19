2acfa17a9203e176f0838a1b98681edbe5b3c274
Lauren Alesia Covington <lacovington16@gmail.com>
2021-08-18 17:12:54 -0700

    comments in catList about why single deployed page won't load

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -5,6 +5,16 @@ import PropTypes from 'prop-types';
 import './CategoryList.css';
 
 const CategoryList = (props) => { 
+    // shows that categoriesData is an arr like it's supposed to be for .map to run...
+    // only guess is that when the page first loads, catData is at 0 bc it hasn't loaded the categories yet and .map returns undefined or whatever..
+    console.log('whats there at first, type: ', typeof(props.categoriesData));
+    console.log('whats there at first, actual: ', props.categoriesData);
+
+    const categoriesData = Array.from(props.categoriesData);
+
+    console.log('post array.from type: ', typeof(categoriesData));
+    console.log('post array.from actual: ', (categoriesData));
+    console.log('type for compare: ', typeof([1,2,3]));
     
     const listofCategories = props.categoriesData.map((category) => {
         return (
